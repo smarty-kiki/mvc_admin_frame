6823c9965908a0848659e418b1fb565fdc85b17f
yaoyang <kiki@smarty.so>
2023-07-22 09:22:55 +0800

    Revert "更新开发环境初始化时的数据库授权语句"
    This reverts commit 46f0d688d732230a83d48677b7f867b1bc63ae12.

--- project/tool/development/after_env_start.sh
+++ project/tool/development/after_env_start.sh
@@ -12,13 +12,10 @@ chown www-data:www-data /tmp/php_module.log
 mysql -e "create database \`default\`;\
     GRANT ALL PRIVILEGES ON *.* TO 'default'@'%' IDENTIFIED BY 'password';\
     GRANT ALL PRIVILEGES ON *.* TO 'default'@'localhost' IDENTIFIED BY 'password';\
-    GRANT ALL PRIVILEGES ON *.* TO 'default'@'172.17.0.1' IDENTIFIED BY 'password';\
-    FLUSH PRIVILEGES;"
+    FLUSH PRIVILEGES"
 
 ENV=development php $ROOT_DIR/public/cli.php migrate:install
 ENV=development php $ROOT_DIR/public/cli.php migrate
 ENV=development php $ROOT_DIR/public/cli.php migrate -tmp_files
-
-ENV=development php $ROOT_DIR/public/cli.php account:add  --name=kiki --email=123@qq.com --password=123 --is_admin=YES
 
 echo ". $ROOT_DIR/project/config/development/bash/cli_complete.bash" >> ~/.bashrc
